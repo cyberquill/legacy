f6de603ac2c0dc83e37c01ab8860af11bff6dbc4
brij <brijgopalbharadwaj1999@gmail.com>
2020-06-13 22:32:29 +0530

    Updates

---
+++ precache-manifest.4220ee1c8fa626118ae0d7c92b4df276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ca82b40e65b886639cf08affe30f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "558908d295b425cec04c",
+    "url": "/static/css/main.b4fb3c53.chunk.css"
+  },
+  {
+    "revision": "53472e651ad4a6ebb3bd",
+    "url": "/static/js/2.7fb9eff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7fb9eff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "558908d295b425cec04c",
+    "url": "/static/js/main.67cfae23.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
