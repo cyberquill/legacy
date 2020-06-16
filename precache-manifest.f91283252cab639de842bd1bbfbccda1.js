2b8ab39e89eee2df355690f1ef2d186f33d13ded
Brijgopal Bharadwaj <brijgopalbharadwaj1999@gmail.com>
2020-06-16 20:23:48 +0530

    Updates

---
+++ precache-manifest.f91283252cab639de842bd1bbfbccda1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e39ac64becbde465c7c536bbf58c32e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b9c02b3b4488add236",
+    "url": "/static/css/main.9e5377a3.chunk.css"
+  },
+  {
+    "revision": "7a0150f5c65e77182da8",
+    "url": "/static/js/2.c1384442.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1384442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b9c02b3b4488add236",
+    "url": "/static/js/main.3a25280a.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "7e57dbb0d775d1d0d80765b927306c0e",
+    "url": "/static/media/Elianto.7e57dbb0.otf"
+  },
+  {
+    "revision": "572ff030012244f144c2cbcf58b24f1e",
+    "url": "/static/media/landing_planet.572ff030.png"
+  }
+]);
